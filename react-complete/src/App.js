6b93e041064d328429ece046ef39a16f6568f89f
Bhumi <bhumisaxena0223@gmail.com>
2021-02-18 19:48:16 +0530

    React Hooks examples

--- react-complete/src/App.js
+++ react-complete/src/App.js
@@ -1,40 +1,42 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Person from './Person/Person.js';
 
-class App extends Component {
-  state = {
+const app = () => {
+
+  const [personsState, setPersonsState] = useState({
     persons: [
-      {name: 'Max', age: 28},
-      {name: 'Bhumi', age: 29},
-      {name: 'Steph', age: 9},
-    ]
-  }
-
-  //Event Handler, a class property which have a function which can execute
-  swithNameHandler = () => {
-    // console.log("ckjdhud ejkuihm");
-    // this.state.persons[0].name = 'Maximillian'; // Don't use this
-    this.setState({
-      persons: [
-      {name: 'Maxton', age: 28},
-      {name: 'Bhumi saxena', age: 29},
-      {name: 'Steph', age: 9
-    },
-    ]
+      { name: 'Max', age: 28 },
+      { name: 'Bhumi', age: 29 }, 
+      { name: 'Steph', age: 9 },
+    ],
   })
+  const [otherState] = useState('value of some other state');
+
+  console.log(personsState, otherState);
+  //Event Handler, a class property which have a function which can execute
+  const swithNameHandler = () => {
+    // console.log("ckjdhud ejkuihm");
+    // personsState.persons[0].name = 'Maximillian'; // Don't use this
+    setPersonsState({
+      persons: [
+        { name: 'Maxton', age: 28 },
+        { name: 'Bhumi saxena', age: 29 },
+        {
+          name: 'Steph', age: 9
+        },
+      ]
+    })
   }
 
-  render() {
-    return (
-      <div className="App">
-       <h1>Hi, I am React Learning guide</h1>
-       <button onClick={this.swithNameHandler}>Switch Name</button>
-       <Person name={this.state.persons[0].name} age={this.state.persons[0].age}></Person>
-       <div>Another Person <Person name={this.state.persons[2].name} age={this.state.persons[2].age}> My work is good</Person></div>
-      </div>
-    );
-  }
+  return (
+    <div className="App">
+      <h1>Hi, I am React Learning guide</h1>
+      <button onClick={swithNameHandler}>Switch Name</button>
+      <Person name={personsState.persons[0].name} age={personsState.persons[0].age}></Person>
+      <div>Another Person <Person name={personsState.persons[2].name} age={personsState.persons[2].age}> My work is good</Person></div>
+    </div>
+  );
 }
 
-export default App;
+export default app;
